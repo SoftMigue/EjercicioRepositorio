2ffc9c1b1695bb6d73f3cb5b4816b10b49d079a2
mmendecu <miguelangel.mendezcumplido.st@nttdata.com>
2021-09-20 14:03:13 +0200

    primera version

---
+++ src/Start.java
@@ -0,0 +1,15 @@
+
+public class Start {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        System.out.println("Hola Mundo");
+        //PROYECTO EN GITHUB DE MIGUEL ÁNGEL MÉNDEZ CUMPLIDO
+
+    }
+
+}
